2b92d80f684c395b0c57f2bf7da8470dc0e806f6
Oscar Grappi <ngrappi@outlook.com>
2023-07-28 03:49:45 -0300

    se agregaron botones para pasar entre las paginas de los productos, un menu desplegable en la nav bar con las categorias y un input, junto con sus verificaciones, para poder elegir la cantidad de los items agregados al carrito

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,10 +1,25 @@
+let totalCheckOut = 0
+
 printCheckOut();
 actualizarCarrito();
+
+let cantidadInputs = document.getElementsByClassName("cantidadProductos")
+for (let i = 0; i < cantidadInputs.length; i++) {
+    let input = cantidadInputs[i]
+    input.addEventListener("change", function (event) {
+        input = event.target
+        if (isNaN(input.value) || input.value <= 0) {
+            input.value = 1
+        }
+        actualizarCarrito();
+    })
+}
+
 
 let botonRemover = document.getElementsByClassName("btn-eliminar")
 for (let i = 0; i < botonRemover.length; i++) {
     boton = botonRemover[i]
-    boton.addEventListener("click", function(event) {
+    boton.addEventListener("click", function (event) {
         let botonClickeado = event.target
         botonClickeado.parentElement.parentElement.remove()
         actualizarCarrito()
@@ -13,11 +28,11 @@ for (let i = 0; i < botonRemover.length; i++) {
 
 let botonBorrarCarrito = document.getElementById("btn-eliminarTodo")
 botonBorrarCarrito.onclick = () => {
-    confirmarEliminar = prompt ("seguro que desea eliminiar el carrito? si/no")
+    confirmarEliminar = prompt("seguro que desea eliminiar el carrito? si/no")
     if (confirmarEliminar == "si") {
         borrarCarrito();
         alert("el carrito fué borrado");
-    }else {
+    } else {
         alert("no se ha borrado el carrito")
     }
 };
@@ -25,11 +40,13 @@ botonBorrarCarrito.onclick = () => {
 let botonComprar = document.getElementById("boton-compra")
 botonComprar.onclick = () => {
     confirmarCompra = prompt("quiere finalizar la compra? tenes un " + document.getElementById("totalCheckOut").innerText + " si/no")
-    if (confirmarCompra == "si") {
-    alert ("gracias por tu compra!")
-    borrarCarrito();
-    actualizarCarrito();
+    if (totalCheckOut == 0 || totalCheckOut < 0) {
+        alert("por favor agregue productos antes de finalizar la compra")
+    } else if (confirmarCompra == "si") {
+        alert("gracias por tu compra!")
+        borrarCarrito();
+        printCheckOut();
     } else {
-        alert ("no se completo la compra, su carrito seguirá guardado")
+        alert("no se completo la compra, su carrito seguirá guardado")
     }
 }
--- JS/main.js
+++ JS/main.js
@@ -33,17 +33,37 @@ function printCheckOut() {
         <th scope="row"> ${i++} </th>
         <td class="nombre-producto"> ${producto.nombre} </td>
         <td class="precio-producto"> $ ${producto.precio} </td>
-        <td class ="cantidad-producto"> ${cantidad} </td>
+        <td class ="cantidad-producto"> 
+        <input class="cantidadProductos" type="number" value="1">
+        </td>
         <td> <button type="button" class="btn btn-danger btn-eliminar" id="btn-eliminar${producto.id}">eliminar</button> </td>
         </tr>`
         tablaCheckOut.appendChild(lista);
+        
     });
 };
 
+// function printCheckOut() {
 
+//     checkout.forEach(producto => {
+
+//         let tablaCheckOut = document.getElementById("tablaCheckOut")
+//         let lista = document.createElement("tr");
+//         lista.classList.add("row-productos")
+//         lista.innerHTML =
+//             `<tr>
+//         <th scope="row"> ${i++} </th>
+//         <td class="nombre-producto"> ${producto.nombre} </td>
+//         <td class="precio-producto"> $ ${producto.precio} </td>
+//         <td class ="cantidad-producto"> ${cantidad} </td>
+//         <td> <button type="button" class="btn btn-danger btn-eliminar" id="btn-eliminar${producto.id}">eliminar</button> </td>
+//         </tr>`
+//         tablaCheckOut.appendChild(lista);
+//     });
+// };
 
 function guardarCarritoJSON() {
-    checkoutArray("listaArray", JSON.stringify(checkout));
+    checkoutArray("listaArray", JSON.stringify(checkout)) || [];
 };
 
 function bajarCarritoJSON() {
@@ -57,22 +77,23 @@ function borrarCarrito() {
     guardarCarritoJSON();
     let tablaCheckOut = document.getElementById("tablaCheckOut")
     tablaCheckOut.remove()
+    totalCheckOut = 0
+    document.getElementById("totalCheckOut").innerText = "total:  $" + totalCheckOut
 }
 
 function actualizarCarrito() {
     let listaCheckOut = document.getElementsByClassName("tablaCheckOut")[0]
     let lineaProductosCheckOut = listaCheckOut.getElementsByClassName("row-productos")
-    let totalCheckOut = 0
+    totalCheckOut = 0
     for (let i = 0; i < lineaProductosCheckOut.length; i++) {
         let lineaProductos = lineaProductosCheckOut[i]
         let productoPrecio = lineaProductos.getElementsByClassName("precio-producto")[0]
-        let productoCantidad = lineaProductos.getElementsByClassName("cantidad-producto")[0]
+        let productoCantidad = lineaProductos.getElementsByClassName("cantidadProductos")[0]
         let precio = parseFloat(productoPrecio.innerText.replace("$", ""))
-        let cantidad = parseFloat(productoCantidad.innerText)
+        let cantidad = productoCantidad.value
         totalCheckOut = totalCheckOut + (precio * cantidad)
     }
     document.getElementById("totalCheckOut").innerText = "total:  $" + totalCheckOut
-
 }
 // -------------------------------- Ejecuciones ---------------------------------- //
 
